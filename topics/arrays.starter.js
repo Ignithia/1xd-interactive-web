d175e443ffc020085bb5663998a745be363ef021
Ignithia <75039029+Ignithia@users.noreply.github.com>
2025-02-10 08:12:24 +0100

    made the exercises

--- topics/arrays.starter.js
+++ topics/arrays.starter.js
@@ -10,27 +10,51 @@
 */
 
 // 1 — create an array `meals` with three of your favorite meals and log it to the console
+let meals = ["Chicken and Rice", "Fries", "Boulette"];
 
 // 2 — create an array `drinks` with three of your favorite drinks and log it to the console
+let drinks = ["Cola", "Iced Tea", "Apple Juice"];
 
 // 3 - create a variable `lastmeal` that stores the last item from the `meals` array
 // hint: use the pop() function - log the result to the console
+let lastmeal = meals.pop();
+console.log(lastmeal);
 
 // 4 - loop over the meals array and print each meal to the console
+for (let i = 0; i < meals.length; i++) {
+  console.log(meals[i]);
+}
 
 // 5 - add a meal called `cereal` to the `meals` array, this time using the push() function, log the variable to the console
+meals.push("Cereal");
 
 // 6 - create a variable `breakfast` and find the position of `cereal` in the `meals` array
 // hint: use the indexOf() function - log the result to the console
+let breakfast = meals.indexOf("Cereal");
+console.log(breakfast);
 
 /* CHALLENGE :: TIPJAR */
 // 7 - create an array `tipjar` that will contain all our tips, add the following tips: 1, 5, 2, 2, 1
+let tipjar = [1, 5, 2, 2, 1];
 
 // 8 - create a function `tip` that will add a tip to the `tipjar` array
 // hint: the function should take a parameter `amount` and add it to the `tipjar` array via `push()`
+function tip(amount) {
+  tipjar.push(amount);
+}
 
 // 9 - call the function with a tip amount of 2 and log the `tipjar` array to the console
+tip(2);
 
 // 10 - create a function `total` that will calculate the total amount of the `tipjar` array
 // hint: use a loop to iterate over the array and add the values together, the function should return the result
 // log the total value of the tips to the console
+
+function total() {
+  let total = 0;
+  for (let i = 0; i < tipjar.length; i++) {
+    total += tipjar[i];
+  }
+  return total;
+}
+console.log(total());
--- topics/dom.starter.js
+++ topics/dom.starter.js
@@ -12,17 +12,35 @@ ______ ________  ___
 // select the second paragraph and figure out a way to make the backround color yellow
 // hint: use the style property and .backgroundColor
 // hint: use the querySelector or getElementById function
+document.getElementById("paragraph2").style.backgroundColor = "yellow";
 
 // 2 - select the element with id #paragraph3 and change the text to "Never gonna run around and desert you"
 // hint: use the innerHTML property
+document.getElementById("paragraph3").innerHTML =
+  "Never gonna run around and desert you";
 
 // 3 - get multiple elements by class
 // select all the span elements by their classname and log the result to your console, what type of object is it?
 // next, loop over the elements and change the innerHTML to another emoji
 // hint: use the querySelectorAll function, use the .forEach method (or a for loop), and the .innerHTML property
+let spans = document.querySelectorAll(".span");
+// next, loop over the elements and change the innerHTML to another emoji
+array.forEach((spans) => {
+  spans.innerHTML = "😄";
+});
 
 // 4 - change the CSS class
 // select the element with the id "message" and change the css class to "error"
 // hint: use the setAttribute function, or use the className property, or use the classList property
+document.getElementById("message").className = "error";
 
 // render a ul with li elements
+
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
+const items = ["one", "two", "three"];
+items.forEach((item) => {
+  const li = document.createElement("li");
+  li.innerHTML = item;
+  ul.appendChild(li);
+});
